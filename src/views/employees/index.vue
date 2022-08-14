18b73d920ffce33c45804b9459ec6312fd6b00ce
limengting <1443848358@qq.com>
2022-08-14 14:55:24 +0800

    完成员工的导入导出功能

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -10,7 +10,9 @@
             @click="$router.push('/import')"
             >导入</el-button
           >
-          <el-button size="small" type="danger">导出</el-button>
+          <el-button size="small" type="danger" @click="exportExcel"
+            >导出</el-button
+          >
           <el-button size="small" type="primary" @click="dialogVisible = true"
             >新增员工</el-button
           >
@@ -98,6 +100,7 @@
 import {getEmployeesInfoApi, delEmployee} from '@/api/employees'
 import employees from '@/constant/employees'
 import AddEmployee from '@/views/employees/components/AddEmployee'
+const {exportExcelMapPath, hireType} = employees
 export default {
   data() {
     return {
@@ -145,6 +148,34 @@ export default {
       await delEmployee(id)
       this.$message.success('删除成功')
       this.getEmployeesInfo()
+    },
+    async exportExcel() {
+      const {export_json_to_excel} = await import('@/vendor/Export2Excel')
+      const {rows} = await getEmployeesInfoApi({
+        page: 1,
+        size: this.total,
+      })
+      const header = Object.keys(exportExcelMapPath) //获得表头数组
+      const data = rows.map((item) => {
+        return header.map((h) => {
+          if (h === '聘用形式') {
+            const findItem = hireType.find((hire) => {
+              return hire.id === item[exportExcelMapPath[h]]
+            })
+            return findItem ? findItem.value : '未知'
+          } else {
+            return item[exportExcelMapPath[h]]
+          }
+        })
+      })
+
+      export_json_to_excel({
+        header, //表头 必填
+        data, //具体数据 必填
+        filename: '员工列表', //非必填
+        autoWidth: true, //非必填
+        bookType: 'xlsx', //非必填
+      })
     },
   },
 }
